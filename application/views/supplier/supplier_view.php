9d9387e7c5ad6e372146ab00494ba70e13b88313
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2018-11-03 17:04:58 -0700

    Update application/views/supplier/supplier_view.php

--- application/views/supplier/supplier_view.php
+++ application/views/supplier/supplier_view.php
@@ -5,14 +5,21 @@ $this->load->view('parts/top_bar');
  ?> 
 
 <div class="row">
-    <div class="box-widget widget-module">
-        <div class="widget-head clearfix">
-            <span class="h-icon"><i class="fa fa-bars"></i></span>
-            <h4>Supplier</h4>
-        </div>
-        <div class="widget-container">
-            <div class="widget-block">
-                <div class="full-tab-container">
+        <div class="box-widget tab-widget">
+            <div class="widget-head clearfix">
+                <ul class="nav nav-tabs pull-right">
+                    <li class="active"><a href="#detail_data_supplier" data-toggle="tab">Detail Data</a></li>
+                    <li><a href="#list_barang" data-toggle="tab">Daftar Produk</a></li>
+                    <!-- <li><a href="#profile" data-toggle="tab">Planogram</a></li> -->
+                </ul>
+                <span class="h-icon"><i class="fa fa-briefcase"></i></span>
+                <h4>Pengaturan Supplier</h4>
+            </div>
+            <div class="widget-container">
+                <div class=" widget-block">
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="penjualan">
+                            <div class="full-tab-container">
                     <button class="btn btn-success"><span class="fa fa-save"></span> Simpan</button>
                     <button class="btn btn-warning"><span class="fa fa-refresh"></span> Refresh</button>
                     <button class="btn btn-primary" id="btnCariBarang"><span class="fa fa-search"></span> Cari Supplier</button>
@@ -54,95 +61,71 @@ $this->load->view('parts/top_bar');
                         <div class="col-md-4">
                             <label><span style="color:red">* </span>Alamat Supplier</label>
                             <textarea class="form-control" style="height: 200px"></textarea>
+                        </div>  
+                    </div>
+                    <div class="col-md-12">
+                        <h4>Informasi Kontak</h4>
+                        <div class="col-md-4">
+                            <label><span style="color:red">* </span>No Telp</label>
+                            <input type="text" class="form-control"> 
                         </div>
-                        
+                        <div class="col-md-4">
+                            <label><span style="color:red">* </span>Email</label>
+                            <input type="text" class="form-control"> 
+                        </div>
+                        <div class="col-md-4">
+                            <label><span style="color:red">* </span>No Handphone</label>
+                            <input type="text" class="form-control"> 
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <h4>Informasi Bank</h4>
+                        <div class="col-md-4">
+                            <label><span style="color:red">* </span>Nama Bank</label>
+                            <select id="satuan" class="form-control col-md-5">
+                                <option>
+                                </option>
+                            </select>
+                        </div>
+                        <div class="col-md-4">
+                            <label><span style="color:red">* </span>No Rekening</label>
+                            <input type="text" class="form-control"> 
+                        </div>
+                        <div class="col-md-4">
+                            <label><span style="color:red">* </span>Nama Pemilik</label>
+                            <input type="text" class="form-control"> 
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-        <div class="box-widget tab-widget">
-            <div class="widget-head clearfix">
-                <ul class="nav nav-tabs pull-right">
-                    <li class="active"><a href="#penjualan" data-toggle="tab">Penjualan</a></li>
-                    <li><a href="#setting_harga" data-toggle="tab">Setting Harga</a></li>
-                    <li><a href="#persediaan" data-toggle="tab">Persediaan</a></li>
-                    <!-- <li><a href="#profile" data-toggle="tab">Planogram</a></li> -->
-                </ul>
-                <span class="h-icon"><i class="fa fa-briefcase"></i></span>
-                <h4>Pengaturan Produk</h4>
-            </div>
-            <div class="widget-container">
-                <div class=" widget-block">
-                    <div class="tab-content">
-                        <div class="tab-pane active" id="penjualan">
+                        </div>
+                        <div class="tab-pane" id="list_barang">
                             <div class="row">
-                               <div class="col-md-6">
-                                    <table class="table table-bordered">
-                                        <tr>
-                                            <th>Penjualan Terakhir</th>
-                                            <th>:</th>
-                                            <td>2018-10-11</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Margin</th>
-                                            <th>:</th>
-                                            <td>25%</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Promo</th>
-                                            <th>:</th>
-                                            <td>Aktif</td>
-                                        </tr>
-                                    </table>
-                                    <hr>
+                                <div class="col-md-4">
+                                    <label>Tipe :</label>
+                                    <select class="form-control">
+                                        <option>Purchase Order</option>
+                                        <option>Barang Dikirim</option>
+                                        <option>Barang Diterima</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                
+                                <div class="col-md-12">
                                     <table class="table table-bordered">
                                         <thead>
                                             <tr>
-                                                <th>Tanggal</th>
-                                                <th>Qty</th>
+                                                <th>Barcode</th>
+                                                <th>Nama Barang</th>
+                                                <th>Stok (D)</th>
+                                                <th>Stok (W)</th>
                                                 <th>Harga Beli</th>
                                                 <th>Harga Jual</th>
-                                                <th>Margin</th>
-                                                <th>Margin (%)</th>
+                                                <th>Margin %</th>
                                             </tr>
                                         </thead>
                                     </table>
-                                </div>
-                                <div class="col-md-6">
-                                    Grafik
-                                </div> 
-                            </div>
-                        </div>
-                        <div class="tab-pane" id="setting_harga">
-                            <div class="block-content">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <label>Harga Beli</label>
-                                        <input type="text" class="form-control">
-                                        <label>Harga Jual</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label>Harga Beli Terakhir</label>
-                                        <input type="text" class="form-control" disabled>
-                                        <label>Harga Jual Terakhir</label>
-                                        <input type="text" class="form-control" disabled>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="tab-pane" id="persediaan">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <label>Stok Gudang</label>
-                                    <input type="number" class="form-control">
-                                    <label>Stok Display</label>
-                                    <input type="number" class="form-control">
-                                    <label>Stok Minimal</label>
-                                    <input type="number" class="form-control">
-                                    <label>Stok Maksimal</label>
-                                    <input type="number" class="form-control">
                                 </div>
                             </div>
                         </div>
